b9515540b2d1389429852d35a76da4ca9803e469
carthioli <carthivie@gmail.com>
2021-11-05 18:21:31 -0300

    finaliza 1ª parte
    estruturado em html e bootstrap, criada sessão para receber os dados.

---
+++ apuracao.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link hel="stylesheet" href="css/style.css">
+  <title>Votação</title>
+  <style>
+
+  </style>  
+</head>
+<body>
+  <header>
+  </header>
+  <main>
+  <div class="container-fluid bg-secondary mb-5">
+    <h1 class="d-flex justify-content-center text-body">APURAÇÃO</h1>
+  </div>  
+  <div class="container bg-secondary p-5 rounded">
+    <div class="">
+
+    </div>  
+  </div> 
+       
+  </main>  
+  <footer>
+  </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,172 @@
+<?php
+    session_start();
+
+    
+
+    $candidato1 = 1;
+    if(isset($_POST['nomeeleitor'])){
+      $nomeeleitor = $_POST['nomeeleitor'];
+    }  
+    if(isset($_POST['branco'])){
+      $voto = ("branco");
+    }
+    if(isset($_POST['naovotar'])){
+      $voto = ("naovotou");
+    }
+    if(isset($_POST['primeirocandidato'])){
+      $voto = ("candidato 1");
+    }
+    if(isset($_POST['segundocandidato'])){
+      $voto = ("candidato 2");
+    }
+    if(isset($_POST['terceirocandidato'])){
+      $voto = ("candidato 3");
+    }
+    $_SESSION['voto'][] = $voto;
+     
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <title>Votação</title>
+  <style>
+
+  .grid {
+  display: grid;
+  grid-template-columns: 6fr 6fr 6fr;
+  grid-template-areas: "g1 g2 g3";
+  height: 550px;
+  }
+  .g1 {
+    grid-area: g1;
+    
+  }
+  .g2 {
+    grid-area: g2;
+  }
+  .g3 {
+    grid-area: g3;
+  }
+
+  </style>  
+</head>
+<body>
+  <header>
+  </header>
+  <main>
+  <div class="container-fluid bg-secondary mb-5">
+    <h1 class="d-flex justify-content-center text-body">REALIZE SEU VOTO</h1>
+  </div>  
+  <div class="container-fluid mt-2">
+      <div class="grid">
+        <div class="grid-item g1 mr-2 bg-secondary rounded h-50">
+          <div class="d-flex justify-content-center h-50">
+            <img type="image" src="img/candidato1.jpeg" width="120px" height="120px" class="p-1 mt-3 bg-info rounded-circle">
+          </div>
+          <h1 class="d-flex justify-content-center text-light mt-4">CANDIDATO 1</h1> 
+            <div class="btn d-flex justify-content-center">
+                <button type="button" class="btn btn-success text-white m-1" data-toggle="modal" data-target="#primeirovoto">VOTAR</button>  
+            <div class="modal fade" id="primeirovoto">
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h4 class="modal-title">INFORME O ELEITOR:</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>                
+                  <div class="modal-body">
+                  <form method="POST" action="index.php">
+                    <input type="text" class="form-control" placeholder="Digite o nome do ELEITOR:" name="nomeeleitor">
+                  </div>
+                  <div class="modal-footer">
+                      <input type="hidden" name="primeirocandidato">
+                      <button type="submit" class="btn btn-success">FINALIZAR</button>
+                      <a class="text-decoration-none text-body" href="index.php"><button type="button" class="btn btn-warning ml-2">CANCELAR</button></a>
+                    </form>  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="grid-item g2 mr-2 bg-secondary rounded h-50">   
+          <div class="d-flex justify-content-center h-50">
+            <img type="image" src="img/candidato2.jpg" width="120px" height="120px" class="p-1 mt-3 bg-info rounded-circle">
+          </div>
+          <h1 class="d-flex justify-content-center text-light mt-4">CANDIDATO 2</h1> 
+            <div class="btn d-flex justify-content-center">
+                <button type="button" class="btn btn-success text-white m-1" data-toggle="modal" data-target="#segundovoto">VOTAR</button>  
+            <div class="modal fade" id="segundovoto">
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h4 class="modal-title">INFORME O ELEITOR:</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>                
+                  <div class="modal-body">
+                  <form method="POST" action="index.php">
+                    <input type="text" class="form-control" placeholder="Digite o nome do ELEITOR:" name="nomeeleitor">
+                  </div>
+                  <div class="modal-footer">
+                      <input type="hidden" name="segundocandidato">
+                      <button type="submit" class="btn btn-success">FINALIZAR</button>
+                      <a class="text-decoration-none text-body" href="index.php"><button type="button" class="btn btn-warning ml-2">CANCELAR</button></a>
+                    </form>  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="grid-item g3 bg-secondary rounded h-50">
+          <div class="d-flex justify-content-center h-50">
+            <img type="image" src="img/candidato3.jpg" width="120px" height="120px" class="p-1 mt-3 bg-info rounded-circle">
+          </div>
+          <h1 class="d-flex justify-content-center text-light mt-4">CANDIDATO 3</h1> 
+            <div class="btn d-flex justify-content-center">
+                <button type="button" class="btn btn-success text-white m-1" data-toggle="modal" data-target="#terceirovoto">VOTAR</button>  
+            <div class="modal fade" id="terceirovoto">
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h4 class="modal-title">INFORME O ELEITOR:</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>                
+                  <div class="modal-body">
+                  <form method="POST" action="index.php">
+                    <input type="text" class="form-control" placeholder="Digite o nome do ELEITOR:" name="nomeeleitor">
+                  </div>
+                  <div class="modal-footer">
+                      <input type="hidden" name="terceirocandidato">
+                      <button type="submit" class="btn btn-success">FINALIZAR</button>
+                      <a class="text-decoration-none text-body" href="index.php"><button type="button" class="btn btn-warning ml-2">CANCELAR</button></a> 
+                    </form>  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>  
+  <div class="container bg-secondary p-3 border rounded col-3">
+    <div class="botaoform d-flex justify-content-center">
+      <form method="POST" action="index.php">    
+        <button class="btn btn-light border ml-2 mr-2" name="branco">BRANCO</button> 
+        <button class="btn btn-danger ml-2" name="naovotar">NÃO VOTAR</button>   
+      </form>  
+    </div>  
+  </div>
+       
+  </main>  
+  <footer>
+  </footer>
+</body>
+</html>
