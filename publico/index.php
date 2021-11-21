b28bf914e57b71350f29805b3477a267ca854a63
carthioli <91908136+carthioli@users.noreply.github.com>
2021-11-21 14:14:33 -0300

    .

--- publico/index.php
+++ publico/index.php
@@ -1,7 +1,9 @@
 <?php 
     include "\\..\\controle\\conexao.php";
     $query = pg_query("SELECT id, nome, partido
-                       FROM candidato;")
+                       FROM candidato
+                      ");
+    $candidato = pg_fetch_assoc($query);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
