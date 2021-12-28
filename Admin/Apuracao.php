908d02b4ddd1e26f9dedbaa9ae0af911c1f3b306
carthioli <carthivie@gmail.com>
2021-12-28 18:30:38 -0300

    adicionado grid / para colocar os resultados dinamicos

--- Admin/Apuracao.php
+++ Admin/Apuracao.php
@@ -8,68 +8,84 @@
     <div class="container-fluid bg-light border-bottom mb-5">
       <h1 class="d-flex justify-content-center text-body">APURAÇÃO</h1>
     </div>  
-    <div class="float-left col-2 ml-5 border">
-      <div class="mt-4 w-100" id="mostraResultados">
-        <table class="table table-striped table-bordered  w-10">
-          <thead>
-            <th class="text-center">ID</th>
-            <th class="text-center">CANDIDATO</th>
-            <th class="text-center">VOTOS</th>
-          </thead>
-          <tbody id="tbody" class="">
+ 
+    <div class="grid">
+      <div class="grid-item ml-5 mr-5 border-right g1">
+        <div id="mostra">
+          <div class="float-left col-10 ml-5 border" id="mostraResultados">
+            <div class="mt-4 w-100">
+                <label>ID:</label>
+                <p id="idCand"></p>  
+          
+                <label>CANDIDATOS:</label>
+                <p id="nomeCand"></p>  
 
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <div class="container bg-light border-left border-top border-right rounded-top mt-2 col-3">
-      <h3 class="text-center mt-3 text-body">MAIS VOTADO!</h3>
-    </div>  
+                <label>TOTAL VOTOS:</label>
+                <p id="qtdVoto"></p>
 
-    <div class="container bg-light border-left border-bottom border-right col-3 candidato">
-      <div class="d-flex justify-content-center h-50">
-        <img width="70px" height="70px" class="p-1 mt-3 bg-info rounded-circle">
-      </div>
-      
-        <div class="container">
-          <div class="row">
-            <div class="col-2">  
-              <h6 class="text-left text-body ">NOME DO CANDIDATO:</h6><br>
-              <h6 class="text-left text-body ">QUANTIDADE DE VOTOS:</h6>
+              <!--<table class="table table-striped table-bordered  w-10">
+                <thead>
+                  <th class="text-center">ID</th>
+                  <th class="text-center">CANDIDATO</th>
+                  <th class="text-center">VOTOS</th>
+                </thead>
+                <tbody id="tbody" class="">
+
+                </tbody>
+              </table>-->
             </div>
-            <div class="col-4 mt-1">
-              <p class="text-body mt-1 text-uppercase" id="nomeMaisVotado">INDEFINIDO</p><br>
-              <p class="text-body mt-1 qntvoto" id="qtdVotosMaisVotado">INDEFINIDO</p>
+          </div>
+        </div>
+      </div> 
+
+      <div class="grid-item border-left g2 ">
+        <div class="container bg-light border-left border-top border-right rounded-top mt-2 col-4 ">
+          <h3 class="text-center mt-3 text-body">MAIS VOTADO!</h3>
+        </div>  
+
+        <div class="container bg-light border-left border-bottom border-right col-4 candidato">
+          <div class="d-flex justify-content-center h-50">
+            <img width="70px" height="70px" class="p-1 mt-3 bg-info rounded-circle">
+          </div>
+          
+            <div class="d-flex justify-content-center">
+              <div class="row">
+                <div class="col-2 ">  
+                  <h6 class="text-left text-body ">NOME DO CANDIDATO:</h6><br>
+                  <h6 class="text-left text-body ">QUANTIDADE DE VOTOS:</h6>
+                </div>
+                <div class="col-4 mt-1">
+                  <p class="text-body mt-1 text-uppercase" id="nomeMaisVotado">INDEFINIDO</p><br>
+                  <p class="text-body mt-1 qntvoto" id="qtdVotosMaisVotado">INDEFINIDO</p>
+                </div>  
+              </div>
+            </div>
+            </div>
+            <div class="container col-4 mt-3 rounded bg-light border outro">
+              <p class="text-center text-body mt-4">OUTROS VOTOS:</p>
+                <div class="container mt-3">
+                  <div class="row ">
+                    <div class="col-2 ">  
+                      <h5 class="text-left text-body mt-4">TOTAL DE VOTOS:</h5>
+                    </div>
+                    <div class="col-4">
+                      <h4 class="text-body mt-3" id="total">0</h4>
+                    </div>  
+                  </div>
+                </div>
+            </div>  
+            <div class="container rounded-top mt-2 col-3">
+              <div class="d-flex justify-content-center mt-2">
+                <div class="p-1">
+                  <form>
+                    <button type="button" id="verificar" class="btn btn-success text-body">VERIFICAR</button>
+                  </form>
+                </div>
+              </div>     
             </div>  
           </div>
         </div>
-    </div>
-    <div class="container col-3 mt-3 rounded bg-light border outro">
-      <p class="text-center text-body mt-4">OUTROS VOTOS:</p>
-        <div class="container mt-3">
-          <div class="row ">
-            <div class="col-2 ">  
-              <h5 class="text-left text-body mt-4">TOTAL DE VOTOS:</h5>
-            </div>
-            <div class="col-4">
-              <h4 class="text-body mt-3" id="total">0</h4>
-            </div>  
-          </div>
-        </div>
-    </div>  
-    <div class="container rounded-top mt-2 col-3">
-      <div class="d-flex justify-content-center mt-2">
-        <div class="p-1">
-          <form>
-            <button type="button" id="verificar" class="btn btn-success text-body">VERIFICAR</button>
-          </form>
-        </div>
-      </div>     
-    </div>  
-    
   </main>  
-  <script src="../javascript/script.js"></script>
-  <footer>
-  </footer>
+    <script src="../javascript/script.js"></script>
 </body>
 </html>
--- javascript/script.js
+++ javascript/script.js
@@ -1,99 +1,94 @@
-	$('#confirma').click(function(){
+$('#confirma').click(function(){
 		
-		var eleitor = $('#nomeeleitor').val()
-		var titulo = $('#titulo').val()
-		var candidato = $('#candidato').val()
+	var eleitor = $('#nomeeleitor').val()
+	var titulo = $('#titulo').val()
+	var candidato = $('#candidato').val()
 
-		$("#mostraeleitor").html("Eleitor: " + eleitor);
-		$("#mostratitulo").html("Titulo: " + titulo);
-		$("#mostracandidato").html("Candidato: " + candidato);
-	});
-	$('#finalizar').click(function(){
-		
-		if($('#finalizar').val() == 'Finalizar...'){
-			return(false);
-		}
-		$.ajax({
-			url: 'computavoto.php',
-			type: 'post',
-			dataType: 'json',
-			data: {
-				'metodo' : $('#metodo').val(),
-			      'nome' : $('#nomeeleitor').val(),
-				'titulo' : $('#titulo').val(),
-			 'candidato' : $('#candidato').val()
-			}
-		}).success(function(data){
-			if(data.status == false){
-				$('#mostrar').attr('class', 'd-flex justify-content-center text-success')
-			}else{
-				$('#mostrar').attr('class', 'd-flex justify-content-center text-danger')
-			}
-			$("#mostrar").html(data.message);
-							
-		});
-		$('#close').click()
-		$('#finalizar').attr('data-dismiss', 'modal')
-	})
-	$('#close').click(function(){
-		$('#metodo').val('formulario-ajax');
-		$('#nomeeleitor').val('');
-		$('#titulo').val('');
-     	$('#candidato').val('');
-		$('#close').attr('data-dismiss', 'modal')
-	});
-
-	$('#cancelar').click(function(){
-
-		$('#metodo').val('formulario-ajax');
-		$('#nomeeleitor').val('');
-		$('#titulo').val('');
-     	$('#candidato').val('');
-		$('#cancelar').attr('data-dismiss', 'modal')
-
-	});
-	$('#verificar').click(function(){
-		$.ajax({
-			url: 'BuscaApuracao.php',
-			type: 'post',
-			dataType: 'json',
-			data: {
-				'busca' : 'aa'
-			}
-		}).success(function(data){
-			$('#total').html(data.busca)
-			$('#nomeMaisVotado').html(data.ultimoMaisVotado['nome'])
-			$('#qtdVotosMaisVotado').html(data.ultimoMaisVotado['id_candidato'])
-
-			var arr = data.candidatos		
-
-			let tbody = $('#tbody').get(0)
-
-        	tbody.innerText = ""
-
-			for (let i = 0; i < 5; i++) {
-				arr.id = (i + 1);
-  				id = arr[i]['id']
-				nome = arr[i]['nome']  
-				totalVotos = arr[i]['totalVotos']
-				
-				  let tr = tbody.insertRow()
-
-				  let td_id = tr.insertCell()
-				  let td_candidato = tr.insertCell()
-				  let td_totalVotos = tr.insertCell()
+	$("#mostraeleitor").html("Eleitor: " + eleitor);
+	$("#mostratitulo").html("Titulo: " + titulo);
+	$("#mostracandidato").html("Candidato: " + candidato);
+});
+$('#finalizar').click(function(){
 	
-				  td_id.innerText = id
-				  td_candidato.innerText = nome
-				  td_totalVotos.innerText = totalVotos
-				  
-			}
-
-			$('#verificar').html('ATUALIZAR');
-			
-		});
-	});  
-
-	function enviar( id_candidato ){
-		$('#candidato').val(id_candidato)
+	if($('#finalizar').val() == 'Finalizar...'){
+		return(false);
 	}
+	$.ajax({
+		url: 'computavoto.php',
+		type: 'post',
+		dataType: 'json',
+		data: {
+			'metodo' : $('#metodo').val(),
+			  'nome' : $('#nomeeleitor').val(),
+			'titulo' : $('#titulo').val(),
+		 'candidato' : $('#candidato').val()
+		}
+	}).success(function(data){
+		if(data.status == false){
+			$('#mostrar').attr('class', 'd-flex justify-content-center text-success')
+		}else{
+			$('#mostrar').attr('class', 'd-flex justify-content-center text-danger')
+		}
+		$("#mostrar").html(data.message);
+						
+	});
+	$('#close').click()
+	$('#finalizar').attr('data-dismiss', 'modal')
+})
+$('#close').click(function(){
+	$('#metodo').val('formulario-ajax');
+	$('#nomeeleitor').val('');
+	$('#titulo').val('');
+	 $('#candidato').val('');
+	$('#close').attr('data-dismiss', 'modal')
+});
+
+$('#cancelar').click(function(){
+
+	$('#metodo').val('formulario-ajax');
+	$('#nomeeleitor').val('');
+	$('#titulo').val('');
+	 $('#candidato').val('');
+	$('#cancelar').attr('data-dismiss', 'modal')
+
+});
+$('#verificar').click(function(){
+	$.ajax({
+		url: 'BuscaApuracao.php',
+		type: 'post',
+		dataType: 'json',
+		data: {
+			'busca' : 'aa'
+		}
+	}).success(function(data){
+		$('#total').html(data.busca)
+		$('#nomeMaisVotado').html(data.ultimoMaisVotado['nome'])
+		$('#qtdVotosMaisVotado').html(data.ultimoMaisVotado['id_candidato'])
+		
+		var arr = data.candidatos		
+
+		for (let i = 0; i < 5; i++) {
+			arr.id = (i + 1);
+			  id = arr[i]['id']
+			nome = arr[i]['nome']  
+			totalVotos = arr[i]['totalVotos']
+
+			var mostra = $('#mostraResultado')
+
+			var divPai = $('#mostra');
+			
+			
+			/*$('#idCand').html(id)
+			$('#nomeCand').html(nome)
+			$('#qtdVoto').html(totalVotos)*/
+			  
+		}
+
+		$('#verificar').html('ATUALIZAR');
+		
+	});
+});  
+
+function enviar( id_candidato ){
+	$('#candidato').val(id_candidato)
+}
